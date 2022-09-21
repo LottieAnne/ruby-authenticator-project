31938a896442cfb6f1d4775a945b37a80cc3308b
cchaae <kkang_chung125@yahoo.co.jp>
2022-09-21 15:25:19 +0900

    feat: エラーメッセージをeach構文の外に出す

--- authenticator.rb
+++ authenticator.rb
@@ -10,11 +10,10 @@ def auth_user(username, password, list_of_users)
     list_of_users.each do |user_record|
         if user_record[:username] == username && user_record[:password] == password
             return user_record
-            break
         else
-            return "Credentials were not correct"
         end
     end
+    "Credentials were not correct"
 end
 
 puts "Welcome to the authenticator"
